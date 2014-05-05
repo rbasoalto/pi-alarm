46f87c4fea87db05924a2069832379c144d5747a
Rodrigo Basoalto <rodrigo.basoalto@inzpiral.com>
2014-05-05 10:56:46 -0400

    Apparently working version of long-polling client

--- pi-alarm.js
+++ pi-alarm.js
@@ -1,7 +1,40 @@
+var http = require('http');
 var gpio = require('pi-gpio');
+var currentState = false;
 
-gpio.open(11, "output", function(err) {
-  gpio.write(11, 1, function(err) {
-    gpio.close(11);
+var actuator = function(outputValue) {
+  console.log('Switching relay: '+outputValue);
+  currentState = outputValue;
+  gpio.open(11, "output", function(err) {
+    gpio.write(11, outputValue, function(err) {
+      gpio.close(11);
+    });
   });
-});
+};
+
+var doPollingReq = function() {
+  console.log('Starting request...');
+  var pollingReqOpts = {
+    host: 'localhost',
+    port: 3000,
+    path: '/alarm',
+    method: 'GET'
+  };
+  var pollingReq = http.request(pollingReqOpts, function(res) {
+    res.on('data', function(data) {
+      console.log('Got data: '+data);
+      var objData = JSON.parse(data);
+      if (objData) {
+        if (objData.value == true) {
+          actuator(true);
+        } else if (objData.value == false) {
+          actuator(false);
+        }
+      }
+    });
+    res.on('close', doPollingReq);
+    res.on('finish', doPollingReq);
+  });
+};
+
+setImmediate(doPollingReq);
